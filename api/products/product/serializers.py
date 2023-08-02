7eec3647e6ff5fe9cd3bc1b09c33da3b9cef3891
baxtikdev <ergashevb111gmail.com>
2023-08-02 21:03:50 +0500

    colour

--- api/products/product/serializers.py
+++ api/products/product/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 from api.products.category.serializers import CategoryCreateSerializer
 from api.products.images.serializers import ProductImageDetailSerializer
-from common.product.models import Product
+from common.product.models import Product, ProductCornerStatus
 
 
 class ProductCreateSerializer(serializers.ModelSerializer):
@@ -17,22 +17,60 @@ class ProductCreateSerializer(serializers.ModelSerializer):
 
 class ProductListSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField()
+    colour = serializers.SerializerMethodField()
     photo_small = serializers.ImageField(read_only=True)
     isLiked = serializers.BooleanField(default=False)
 
     def get_category(self, product):
         return product.category.title if product.category else None
 
+    def get_colour(self, product):
+        colour = None
+        if product.cornerStatus == ProductCornerStatus.CHEAP:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.DISCOUNT:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.EXPENSIVE:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.NEWS:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.POPULAR:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.RECOMMENDATION:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.SPECIAL:
+            colour = ""
+        return colour
+
     class Meta:
         model = Product
         fields = ['id', 'guid', 'category', 'title', 'code', 'price', 'brand', 'size', 'manufacturer', 'photo_small',
-                  'file3D', 'cornerStatus', 'isLiked']
+                  'file3D', 'cornerStatus', 'isLiked', 'colour']
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     category = CategoryCreateSerializer()
+    colour = serializers.SerializerMethodField()
     photo_medium = serializers.ImageField(read_only=True)
     photos = serializers.SerializerMethodField()
+
+    def get_colour(self, product):
+        colour = None
+        if product.cornerStatus == ProductCornerStatus.CHEAP:
+            colour = ""
+        elif product.discount or product.cornerStatus == ProductCornerStatus.DISCOUNT:
+            colour = "#E31E25"
+        elif product.cornerStatus == ProductCornerStatus.EXPENSIVE:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.NEWS:
+            colour = "#FFFFFF"
+        elif product.cornerStatus == ProductCornerStatus.POPULAR:
+            colour = ""
+        elif product.cornerStatus == ProductCornerStatus.RECOMMENDATION:
+            colour = "#FDE9EA"
+        elif product.cornerStatus == ProductCornerStatus.SPECIAL:
+            colour = "#FFBC10"
+        return colour
 
     def get_photos(self, product):
         product_images = product.productImages.all()
@@ -42,4 +80,4 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'guid', 'category', 'code', 'title', 'description', 'price', 'material', 'unit', 'file3D',
-                  'status', 'brand', 'size', 'manufacturer', 'photo_medium', 'photos']
+                  'status', 'brand', 'size', 'manufacturer', 'photo_medium', 'photos', 'colour']
