aa0f035a21fecce8c7685611b784c19fa15af6dd
baxtikdev <ergashevb111@gmail.com>
2023-08-09 22:44:16 +0500

    catalog and portfolio

--- common/catalog/models.py
+++ common/catalog/models.py
@@ -23,4 +23,8 @@ class Catalog(BaseModel):
 
 class CatalogImage(BaseModel):
     catalog = models.ForeignKey(Catalog, related_name='catalogImages', on_delete=models.CASCADE)
-    imageURL = models.URLField()
+    photo = models.ImageField(_("Image of Product"), upload_to='catalogImage', null=True, blank=True)
+    photo_medium = ImageSpecField(source='photo', processors=[ResizeToFill(250, 250)], format='PNG',
+                                  options={'quality': 90})
+    photo_small = ImageSpecField(source='photo', processors=[ResizeToFill(70, 70)], format='PNG',
+                                 options={'quality': 70})
