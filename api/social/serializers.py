e26284cc7274bcd6ad6f36eac190615bf8dc922d
baxtikdev <ergashevb111gmail.com>
2023-08-17 02:56:34 +0500

    filial

--- api/social/serializers.py
+++ api/social/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
+from api.filial.serializers import FilialListSerializer
 from common.social.models import Social
 
 
 class SocialLinksSerializer(serializers.ModelSerializer):
+    filials = serializers.SerializerMethodField()
+
+    def get_filials(self, social):
+        return FilialListSerializer(social.filial.all(), many=True).data
+
     class Meta:
         model = Social
-        fields = ['id', 'guid', 'telegram', 'instagram', 'facebook', 'youtube', 'phone', 'location']
+        fields = ['id', 'guid', 'telegram', 'instagram', 'facebook', 'youtube', 'filials']
