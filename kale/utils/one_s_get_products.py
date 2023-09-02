2257ff180f8a2c4ea5a16ff7f297d82e40a97878
baxtikdev <ergashevb111@gmail.com>
2023-09-02 14:41:35 +0500

    isCart

--- kale/utils/one_s_get_products.py
+++ kale/utils/one_s_get_products.py
@@ -7,20 +7,10 @@ password = "kaleapi"
 
 
 def get_products():
-    sent_sms_base(105, "Keldi", '+998901321921')
     url = "http://94.158.52.249/Base/hs/info/stocks/"
-    response = requests.get(url, auth=(username, password))
-    sent_sms_base(105, "Zapros yubordi", '+998901321921')
-    sent_sms_base(105, 200, '+998901321921')
-    print(response.json())
     try:
-        print(f"Zapros ketdi{response.status_code} {len(response.json().get('Товары'))}")
+        response = requests.get(url, auth=(username, password))
+        return response.json()
     except:
-        sent_sms_base(105, response.status_code, '+998901321921')
-
-    # sent_sms_base(105, f"Zapros ketdi{response.status_code} {len(response.json().get('Товары'))}", '+998901321921')
-    # if response.status_code == 200:
-    return response.json()
-    # else:
-        # print("Error while updating products in kale")
-        # sent_sms_base(105, "Error while updating products in kale", '998901321921')
+        sent_sms_base(105, "Error berdi", '+998901321921')
+        return {"Товары": []}
