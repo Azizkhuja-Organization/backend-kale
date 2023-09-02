5a595607c5b198b9a95150fe5ab60376cba01a03
baxtikdev <ergashevb111@gmail.com>
2023-09-02 14:48:58 +0500

    products

--- kale/utils/one_s_get_products.py
+++ kale/utils/one_s_get_products.py
@@ -8,9 +8,12 @@ password = "kaleapi"
 
 def get_products():
     url = "http://94.158.52.249/Base/hs/info/stocks/"
-    try:
-        response = requests.get(url, auth=(username, password))
-        return response.json()
-    except:
-        sent_sms_base(105, "Error berdi", '+998901321921')
-        return {"Товары": []}
+    # try:
+    response = requests.get(url, auth=(username, password))
+    sent_sms_base(105, response.status_code, '+998901321921')
+    print(response.status_code)
+    print(response.json())
+    return response.json()
+    # except:
+    #     sent_sms_base(105, "Error berdi", '+998901321921')
+    #     return {"Товары": []}
