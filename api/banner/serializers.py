63dfcfacbed0966c2cc76db622ce5b1095515572
baxtikdev <ergashevb111@gmail.com>
2023-09-22 14:58:11 +0500

    banner

--- api/banner/serializers.py
+++ api/banner/serializers.py
@@ -6,34 +6,42 @@ from common.banner.models import Banner, SmallBanner, PointerNumber
 
 class BannerCreateSerializer(serializers.ModelSerializer):
     photo = Base64ImageField()
+    photo2 = Base64ImageField()
+    photo3 = Base64ImageField()
 
     class Meta:
         model = Banner
-        fields = ['id', 'guid', 'photo', 'url']
+        fields = ['id', 'guid', 'photo', 'photo2', 'photo3', 'url']
 
 
 class BannerListSerializer(serializers.ModelSerializer):
     photo_medium = serializers.ImageField(read_only=True)
+    photo_medium2 = serializers.ImageField(read_only=True)
+    photo_medium3 = serializers.ImageField(read_only=True)
 
     class Meta:
         model = Banner
-        fields = ['id', 'guid', 'photo_medium', 'url']
+        fields = ['id', 'guid', 'photo_medium', 'photo_medium2', 'photo_medium3', 'url']
 
 
 class SmallBannerCreateSerializer(serializers.ModelSerializer):
     photo = Base64ImageField()
+    photo2 = Base64ImageField()
+    photo3 = Base64ImageField()
 
     class Meta:
         model = SmallBanner
-        fields = ['id', 'guid', 'photo', 'url']
+        fields = ['id', 'guid', 'photo', 'photo2', 'photo3', 'url']
 
 
 class SmallBannerListSerializer(serializers.ModelSerializer):
     photo_medium = serializers.ImageField(read_only=True)
+    photo_medium2 = serializers.ImageField(read_only=True)
+    photo_medium3 = serializers.ImageField(read_only=True)
 
     class Meta:
         model = SmallBanner
-        fields = ['id', 'guid', 'photo_medium', 'url']
+        fields = ['id', 'guid', 'photo_medium', 'photo_medium2', 'photo_medium3', 'url']
 
 
 class PointerNumberCreateSerializer(serializers.ModelSerializer):
--- common/banner/models.py
+++ common/banner/models.py
@@ -7,11 +7,21 @@ from common.users.base import BaseModel, BaseMeta
 
 
 class Banner(BaseModel):
-    photo = models.ImageField(_("Image of Banner"), upload_to='bannerImage')
+    photo = models.ImageField(_("Image of Banner"), upload_to='bannerImage', null=True, blank=True)
     photo_medium = ImageSpecField(source='photo', processors=[ResizeToFill(1463, 420)], format='PNG',
                                   options={'quality': 100})
     photo_small = ImageSpecField(source='photo', processors=[ResizeToFill(322, 209)], format='PNG',
                                  options={'quality': 70})
+    photo2 = models.ImageField(_("Image of Banner"), upload_to='bannerImage', null=True, blank=True)
+    photo_medium2 = ImageSpecField(source='photo', processors=[ResizeToFill(1463, 420)], format='PNG',
+                                   options={'quality': 100})
+    photo_small2 = ImageSpecField(source='photo', processors=[ResizeToFill(322, 209)], format='PNG',
+                                  options={'quality': 70})
+    photo3 = models.ImageField(_("Image of Banner"), upload_to='bannerImage', null=True, blank=True)
+    photo_medium3 = ImageSpecField(source='photo', processors=[ResizeToFill(1463, 420)], format='PNG',
+                                   options={'quality': 100})
+    photo_small3 = ImageSpecField(source='photo', processors=[ResizeToFill(322, 209)], format='PNG',
+                                  options={'quality': 70})
     url = models.CharField(max_length=250, null=True, blank=True)
 
     class Meta(BaseMeta):
@@ -22,11 +32,21 @@ class Banner(BaseModel):
 
 
 class SmallBanner(BaseModel):
-    photo = models.ImageField(_("Image of SmallBanner"), upload_to='smallBannerImage')
+    photo = models.ImageField(_("Image of SmallBanner"), upload_to='smallBannerImage', null=True, blank=True)
     photo_medium = ImageSpecField(source='photo', processors=[ResizeToFill(967, 265)], format='PNG',
                                   options={'quality': 100})
     photo_small = ImageSpecField(source='photo', processors=[ResizeToFill(322, 209)], format='PNG',
                                  options={'quality': 70})
+    photo2 = models.ImageField(_("Image of SmallBanner"), upload_to='smallBannerImage', null=True, blank=True)
+    photo_medium2 = ImageSpecField(source='photo', processors=[ResizeToFill(967, 265)], format='PNG',
+                                   options={'quality': 100})
+    photo_small2 = ImageSpecField(source='photo', processors=[ResizeToFill(322, 209)], format='PNG',
+                                  options={'quality': 70})
+    photo3 = models.ImageField(_("Image of SmallBanner"), upload_to='smallBannerImage', null=True, blank=True)
+    photo_medium3 = ImageSpecField(source='photo', processors=[ResizeToFill(967, 265)], format='PNG',
+                                   options={'quality': 100})
+    photo_small3 = ImageSpecField(source='photo', processors=[ResizeToFill(322, 209)], format='PNG',
+                                  options={'quality': 70})
     url = models.CharField(max_length=250, null=True, blank=True)
 
     class Meta(BaseMeta):
