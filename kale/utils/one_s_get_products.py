151bf1a8a337ec84958adaf83230e146c025896d
baxtikdev <ergashevb111@gmail.com>
2023-09-24 00:29:40 +0500

    cartProductQuantity

--- kale/utils/one_s_get_products.py
+++ kale/utils/one_s_get_products.py
@@ -16,9 +16,12 @@ def get_products():
 
 def get_product_photo(code):
     url = f"http://94.158.52.249/Base/hs/info/foto?code={code}"
-    response = requests.get(url, auth=(username, password))
-    data = response.json()
-    base64Photo = data.get('Фото')
-    if base64Photo is None:
+    try:
+        response = requests.get(url, auth=(username, password))
+        data = response.json()
+        base64Photo = data.get('Фото')
+        if base64Photo is None:
+            return None
+        return "data:image/png;base64," + base64Photo.replace("\r", '').replace("\n", '').replace("\'", '')
+    except:
         return None
-    return "data:image/png;base64," + base64Photo.replace("\r", '').replace("\n", '').replace("\'", '')
