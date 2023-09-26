1e92c6d23094895d7d63d1bc090c0f8cc9276b47
baxtikdev <ergashevb111@gmail.com>
2023-09-26 15:08:58 +0500

    add

--- api/products/product/urls.py
+++ api/products/product/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from api.products.product.views import ProductCreateAPIView, ProductListAPIView, ProductDetailAPIView, \
-    ProductUpdateAPIView, ProductDeleteAPIView
+    ProductUpdateAPIView, ProductDeleteAPIView, ProductUpdatesAPIView
 
 app_name = 'product'
 
 urlpatterns = [
     path("-create/", ProductCreateAPIView.as_view(), name="product_create"),
     path("-list/", ProductListAPIView.as_view(), name="product_list"),
+    path("-updates/", ProductUpdatesAPIView.as_view(), name="product_updates"),
     path("-detail/<uuid:guid>/", ProductDetailAPIView.as_view(), name="product_detail"),
     path("-update/<uuid:guid>/", ProductUpdateAPIView.as_view(), name="product_update"),
     path("-destroy/<uuid:guid>/", ProductDeleteAPIView.as_view(), name="product_delete"),
 ]
+
--- api/products/product/views.py
+++ api/products/product/views.py
@@ -1,8 +1,12 @@
+import base64
+
+from django.core.files.base import ContentFile
 from django.db.models import Q, Exists, OuterRef, F
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, ListAPIView, DestroyAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.paginator import CustomPagination
 from api.permissions import IsAdmin
@@ -10,7 +14,8 @@ from api.products.images.serializers import ProductImageCreateSerializer
 from api.products.product.serializers import ProductCreateSerializer, ProductListSerializer, ProductDetailSerializer, \
     ProductUpdateSerializer
 from common.order.models import Wishlist, Comparison, CartProduct
-from common.product.models import Product, ProductImage
+from common.product.models import Product, ProductImage, SubCategory
+from kale.utils.one_s_get_products import get_product_photo, get_products
 
 
 class ProductCreateAPIView(CreateAPIView):
@@ -37,6 +42,81 @@ class ProductCreateAPIView(CreateAPIView):
             if obj:
                 ProductImage.objects.bulk_create(obj)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductUpdatesAPIView(APIView):
+    def get(self):
+        products = get_products()
+        newProducts = []
+        updateProducts = []
+        for product in products.get("Товары"):
+            category_name = product.get("Категория")
+            quantity = product.get("Остаток")
+            code = product.get("Код")
+            price = product.get("Цена")
+            title = product.get("Наименование")
+            unit = product.get("ЕдиницаИзмерения")
+            brand = product.get("ТорговаяМарка")
+            size = product.get("Размеры")
+            description = product.get("Описание")
+            manufacturer = product.get("Производитель")
+            if category_name and code and price > 0 and quantity > 0 and title:
+                category = SubCategory.objects.filter(title_ru=category_name).first()
+                if category is None:
+                    continue
+                pr = Product.objects.filter(code=code).first()
+
+                photo_content = None
+
+                if pr and pr.code == code:  # and pr.quantity < quantity:
+                    if pr.photo is None:
+                        photo = get_product_photo(code)
+                        if photo:
+                            photo_data = photo.split(";base64,")[1]
+                            photo_content = ContentFile(base64.b64decode(photo_data), name=f"{code}_photo.png")
+                    updateProducts.append(Product(
+                        id=pr.id,
+                        subcategory=category,
+                        # title=title,
+                        title_ru=title,
+                        description_ru=description,
+                        price=price,
+                        # material_ru=material,
+                        unit=unit,
+                        brand=brand,
+                        size=size,
+                        manufacturer_ru=manufacturer,
+                        quantity=quantity,
+                        photo=photo_content
+                    ))
+                elif pr is None:
+                    photo = get_product_photo(code)
+                    if photo:
+                        photo_data = photo.split(";base64,")[1]
+                        photo_content = ContentFile(base64.b64decode(photo_data), name=f"{code}_photo.png")
+                    newProducts.append(Product(
+                        subcategory=category,
+                        code=code,
+                        # title=title,
+                        title_ru=title,
+                        description_ru=description,
+                        price=price,
+                        # material_ru=material,
+                        unit=unit,
+                        brand=brand,
+                        size=size,
+                        manufacturer_ru=manufacturer,
+                        quantity=quantity,
+                        photo=photo_content
+                    ))
+        if newProducts:
+            Product.objects.bulk_create(newProducts)
+        if updateProducts:
+            Product.objects.bulk_update(updateProducts,
+                                        fields=['subcategory', 'title_ru', 'description_ru', 'price', 'unit', 'brand',
+                                                'size',
+                                                'manufacturer_ru', 'quantity'])
+        return Response(status=status.HTTP_200_OK)
 
 
 class ProductListAPIView(ListAPIView):
