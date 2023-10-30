66cf88773c8f8a41a336e417ee8c13a9156e1006
STQT <productgsm@gmail.com>
2023-10-30 12:56:17 +0500

    fix small banner

--- api/banner/views.py
+++ api/banner/views.py
@@ -51,19 +51,6 @@ class SmallBannerListAPIView(ListAPIView):
     serializer_class = SmallBannerListSerializer
     pagination_class = CustomPagination
 
-    # @method_decorator(cache_page(CACHE_TTL))
-    # @method_decorator(vary_on_cookie)
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
 
 class SmallBannerUpdateAPIView(UpdateAPIView):
     queryset = SmallBanner.objects.all()
