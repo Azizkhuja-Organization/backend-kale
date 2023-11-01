2938efe53d9198e7d80a160df63af8ff5fcb21a1
Kairat Sultan <garri.sultonov@mail.ru>
2023-11-01 18:24:12 +0600

    change sentry dsn

--- config/settings/production.py
+++ config/settings/production.py
@@ -131,7 +131,7 @@ LOGGING = {
 
 # Sentry
 # ------------------------------------------------------------------------------
-SENTRY_DSN = "https://d21b385805d6513a799fe3c970f6b8bd@o4504724211105792.ingest.sentry.io/4506094547632128"
+SENTRY_DSN = "https://adefc849f369174ea95dc2a7515b8b86@o4504724211105792.ingest.sentry.io/4506150589693952"
 SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
 sentry_logging = LoggingIntegration(
     level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
